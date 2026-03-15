6edebb4e61a6aa62a94359266799cc20226f5a24
divyam <divyam@gmail.com>
2026-03-15 14:32:06 +0530

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -8,20 +8,22 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from 'path';
+import path from "path";
 import { fileURLToPath } from "url";
 
+dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173" ,
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.CORS_ORIGIN],
     credentials: true,
-  }))
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -29,7 +31,7 @@ app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
-  })
+  }),
 );
 
 // Route
@@ -38,18 +40,12 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-
 // Server static files
 // app.use(express.static(path.join(__dirname, '../frontend/dist')))
 // app.get("/*", (_,res)=> {
 //   res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'))
 // })
 
-
 app.use(errorHandleMiddleware);
-if(process.env.NODE_ENV!=='PRODUCTION'){
-    dotenv.config({path: 'backend/config/config.env'});
-}
-
 
 export default app;
--- frontend/src/app/api.js
+++ frontend/src/app/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL ,
+    baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:7007" ,
     withCredentials: true,
    
     
 })
-console.log("kd",import.meta.env.VITE_BASE_URL)
+// console.log("kd",import.meta.env.VITE_BASE_URL)
 
 export default api;
